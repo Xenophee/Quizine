0a4410a6c59d47c5ed1315f76dee0b559b8301cb
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-11 13:49:23 +0100

    Mise en place du squelette

---
+++ src/main/java/com/dassonville/quizine/QuizineApplication.java
@@ -0,0 +1,13 @@
+package com.dassonville.quizine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dassonville/quizine/QuizineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dassonville.quizine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
