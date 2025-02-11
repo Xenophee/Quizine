fabf6617caf1655676da6f2c563d65363411cc88
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-02-12 00:06:27 +0100

    Ajustement du script de la bdd

--- src/main/resources/bdd.sql
+++ src/main/resources/bdd.sql
@@ -1,22 +1,22 @@
 CREATE SCHEMA IF NOT EXISTS dev;
 SET
-search_path TO dev;
+    search_path TO dev;
 
 ---------------------------------------------------
 -- Créer la table des utilisateurs
 ---------------------------------------------------
 CREATE TABLE users
 (
-    id            UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
-    username      VARCHAR(50)  NOT NULL UNIQUE,
-    email         VARCHAR(150) NOT NULL UNIQUE,
-    password      VARCHAR(255) NOT NULL,
-    registered_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    validated_at  TIMESTAMP,
-    updated_at    TIMESTAMP,
-    connected_at  TIMESTAMP,
-    deleted_at    TIMESTAMP,
-    public        BOOLEAN      NOT NULL DEFAULT TRUE
+    id             UUID                  DEFAULT gen_random_uuid() PRIMARY KEY,
+    username       VARCHAR(50)  NOT NULL UNIQUE,
+    email          VARCHAR(150) NOT NULL UNIQUE,
+    password       VARCHAR(255) NOT NULL,
+    registered_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    validated_at   TIMESTAMP,
+    updated_at     TIMESTAMP,
+    connected_at   TIMESTAMP,
+    deleted_at     TIMESTAMP,
+    public_profile BOOLEAN      NOT NULL DEFAULT TRUE
 );
 
 ---------------------------------------------------
@@ -33,8 +33,8 @@ CREATE TABLE roles
 ---------------------------------------------------
 CREATE TABLE users_roles
 (
-    id_user INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    id_role INTEGER NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
+    id_user INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    id_role INTEGER NOT NULL REFERENCES roles (id) ON DELETE CASCADE,
     PRIMARY KEY (id_user, id_role)
 );
 
@@ -44,8 +44,8 @@ CREATE TABLE users_roles
 CREATE TABLE themes
 (
     id          SERIAL PRIMARY KEY,
-    name        VARCHAR(50) NOT NULL,
-    description VARCHAR(250),
+    name        VARCHAR(50) NOT NULL UNIQUE,
+    description VARCHAR(250)
 );
 
 ---------------------------------------------------
@@ -54,7 +54,7 @@ CREATE TABLE themes
 CREATE TABLE categories
 (
     id          SERIAL PRIMARY KEY,
-    name        VARCHAR(70) NOT NULL,
+    name        VARCHAR(70) NOT NULL UNIQUE,
     description VARCHAR(250),
     id_theme    INTEGER     NOT NULL REFERENCES themes (id) ON DELETE CASCADE
 );
@@ -65,20 +65,29 @@ CREATE TABLE categories
 CREATE TABLE difficulty_levels
 (
     id                  SERIAL PRIMARY KEY,
-    name                VARCHAR(50) NOT NULL,
+    name                VARCHAR(50) NOT NULL UNIQUE,
     max_responses       SMALLINT    NOT NULL,
-    has_timer           BOOLEAN     NOT NULL,
+    timer_seconds       SMALLINT    NOT NULL DEFAULT 0,
     points_per_question SMALLINT    NOT NULL
+);
+
+---------------------------------------------------
+-- Créer la table des statuts
+---------------------------------------------------
+CREATE TABLE statuses
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL UNIQUE
 );
 
 ---------------------------------------------------
 -- Créer la table des quiz
 ---------------------------------------------------
-CREATE TABLE quiz
+CREATE TABLE quizzes
 (
     id          SERIAL PRIMARY KEY,
-    name        VARCHAR(50)  NOT NULL,
-    alt         VARCHAR(150) NOT NULL,
+    title       VARCHAR(100) NOT NULL,
+    alt_image   VARCHAR(150) NOT NULL,
     id_category INTEGER      NOT NULL REFERENCES categories (id) ON DELETE CASCADE,
     id_theme    INTEGER      NOT NULL REFERENCES themes (id) ON DELETE CASCADE
 );
@@ -88,8 +97,9 @@ CREATE TABLE quiz
 ---------------------------------------------------
 CREATE TABLE questions
 (
-    id    SERIAL PRIMARY KEY,
-    title VARCHAR(300) NOT NULL
+    id   SERIAL PRIMARY KEY,
+    text VARCHAR(300) NOT NULL,
+    id_quiz INTEGER NOT NULL REFERENCES quizzes (id) ON DELETE CASCADE
 );
 
 ---------------------------------------------------
@@ -98,21 +108,11 @@ CREATE TABLE questions
 CREATE TABLE answers
 (
     id          SERIAL PRIMARY KEY,
-    title       VARCHAR(150) NOT NULL,
-    is_correct  BOOLEAN      NOT NULL,
-    id_question INTEGER      NOT NULL,
-    FOREIGN KEY (id_question) REFERENCES questions (id)
+    text        VARCHAR(150) NOT NULL,
+    is_correct  BOOLEAN      NOT NULL DEFAULT FALSE,
+    id_question INTEGER      NOT NULL REFERENCES questions (id)
 );
 
----------------------------------------------------
--- Créer la table qui lie les quiz aux questions
----------------------------------------------------
-CREATE TABLE quiz_questions
-(
-    id_quiz     INTEGER REFERENCES quiz (id),
-    id_question INTEGER REFERENCES questions (id),
-    PRIMARY KEY (id_quiz, id_question)
-);
 
 ---------------------------------------------------
 -- Créer la table des tentatives
@@ -121,11 +121,11 @@ CREATE TABLE attempts
 (
     id                  SERIAL PRIMARY KEY,
     attempts_date       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    status              VARCHAR(20)        DEFAULT 'in_progress',
     score               INTEGER   NOT NULL DEFAULT 0,
-    id_quiz             INTEGER   NOT NULL REFERENCES quiz (id) ON DELETE CASCADE,
+    id_quiz             INTEGER   NOT NULL REFERENCES quizzes (id) ON DELETE CASCADE,
     id_difficulty_level INTEGER   NOT NULL REFERENCES difficulty_levels (id) ON DELETE CASCADE,
-    id_user             INTEGER   NOT NULL REFERENCES users (id) ON DELETE SET NULL
+    id_user             INTEGER   NOT NULL REFERENCES users (id) ON DELETE SET NULL,
+    id_status           INTEGER   NOT NULL REFERENCES statuses (id)
 );
 
 ---------------------------------------------------
@@ -138,7 +138,7 @@ CREATE TABLE attempts_responses
     id_attempt    INTEGER NOT NULL REFERENCES attempts (id) ON DELETE CASCADE,
     id_question   INTEGER NOT NULL REFERENCES questions (id) ON DELETE CASCADE,
     id_answer     INTEGER REFERENCES answers (id), -- Null si aucune réponse
-    UNIQUE (attempt_id, question_id, answer_id)
+    UNIQUE (id_attempt, id_question, id_answer)
 );
 
 
@@ -150,15 +150,11 @@ CREATE TABLE attempts_responses
 CREATE INDEX idx_categories_theme ON categories (id_theme);
 
 -- Pour les requêtes de filtrage ou de jointure des quiz par catégorie et thème
-CREATE INDEX idx_quiz_category ON quiz (id_category);
-CREATE INDEX idx_quiz_theme ON quiz (id_theme);
+CREATE INDEX idx_quiz_category ON quizzes (id_category);
+CREATE INDEX idx_quiz_theme ON quizzes (id_theme);
 
 -- Pour accélérer les jointures entre questions et leurs réponses
 CREATE INDEX idx_answers_question ON answers (id_question);
-
--- Pour les requêtes cherchant les questions associées à un quiz
-CREATE INDEX idx_quiz_questions_quiz ON quiz_questions (id_quiz);
-CREATE INDEX idx_quiz_questions_question ON quiz_questions (id_question);
 
 -- Pour les requêtes cherchant les réponses d'une question pour une tentative
 CREATE INDEX idx_attempt_responses_attempt ON attempts_responses (id_attempt);
