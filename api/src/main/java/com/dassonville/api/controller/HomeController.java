3b1a9af53c7681e1938fa84f31924ca920c2fe5b
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-16 11:36:03 +0100

    Ajout d'une redirection pour la doc swagger

--- api/src/main/java/com/dassonville/api/controller/HomeController.java
+++ api/src/main/java/com/dassonville/api/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.dassonville.api.controller;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
+@Hidden
 @RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "Hello World! " + System.getenv("SPRING_PROFILES_ACTIVE");
+    }
+
+    @GetMapping("/doc")
+    public RedirectView redirectWithRedirectView() {
+        return new RedirectView("/swagger-ui/index.html");
     }
 }
