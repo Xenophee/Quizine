1a3ad99148f6556066868b023140167cfc071c22
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-16 15:27:02 +0100

    Restructuration des tests unitaires du service de gestion des thèmes dans des class nested

--- api/src/test/java/com/dassonville/api/service/ThemeServiceTest.java
+++ api/src/test/java/com/dassonville/api/service/ThemeServiceTest.java
@@ -8,6 +8,7 @@ import com.dassonville.api.model.Theme;
 import com.dassonville.api.repository.ThemeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -66,181 +67,209 @@ public class ThemeServiceTest {
         verify(themeRepository).findAll();
     }
 
-    @Test
-    @DisplayName("Récupérer un thème par son ID")
-    public void findById_existingId() {
-        // Given
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.of(theme));
 
-        // When
-        Theme result = themeService.findById(theme.getId());
+    @Nested
+    @DisplayName("Tests pour findById")
+    class FindByIdTests {
 
-        // Then
-        verify(themeRepository).findById(any(Long.class));
-        assertThat(result).isNotNull();
-        assertThat(result).isEqualTo(theme);
-    }
+        @Test
+        @DisplayName("Récupérer un thème par son ID")
+        public void findById_existingId() {
+            // Given
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.of(theme));
 
-    @Test
-    @DisplayName("Récupérer un thème par un ID inexistant")
-    public void findById_nonExistingId() {
-        // Given
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.empty());
+            // When
+            Theme result = themeService.findById(theme.getId());
 
-        // When & Then
-        assertThrows(NotFoundException.class, () -> themeService.findById(theme.getId()));
+            // Then
+            verify(themeRepository).findById(any(Long.class));
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(theme);
+        }
 
-        verify(themeRepository).findById(any(Long.class));
-    }
+        @Test
+        @DisplayName("Récupérer un thème par un ID inexistant")
+        public void findById_nonExistingId() {
+            // Given
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.empty());
 
-    @Test
-    @DisplayName("Créer un nouveau thème")
-    public void create_newTheme() {
-        // Given
-        when(themeRepository.existsByName(any(String.class)))
-                .thenReturn(false);
-        when(themeRepository.save(any(Theme.class)))
-                .thenReturn(theme);
+            // When & Then
+            assertThrows(NotFoundException.class, () -> themeService.findById(theme.getId()));
 
-        // When
-        Theme result = themeService.create(theme);
+            verify(themeRepository).findById(any(Long.class));
+        }
 
-        // Then
-        verify(themeRepository).existsByName(any(String.class));
-        verify(themeRepository).save(any(Theme.class));
-        assertThat(result).isNotNull();
-        assertThat(result).isEqualTo(theme);
-    }
-
-    @Test
-    @DisplayName("Créer un thème déjà existant")
-    public void create_existingTheme() {
-        // Given
-        when(themeRepository.existsByName(any(String.class)))
-                .thenReturn(true);
-
-        // When & Then
-        assertThrows(AlreadyExistException.class, () -> themeService.create(theme));
-
-        verify(themeRepository).existsByName(any(String.class));
-    }
-
-    @Test
-    @DisplayName("Mettre à jour un thème existant")
-    public void update_existingTheme() {
-
-        Theme themeToUpdate = createUpdatedTheme();
-
-        // Given
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.of(theme));
-        when(themeRepository.existsByName(any(String.class)))
-                .thenReturn(false);
-        when(themeRepository.save(any(Theme.class)))
-                .thenReturn(themeToUpdate);
-
-        // When
-        Theme result = themeService.update(theme.getId(), themeToUpdate);
-
-        // Then
-        verify(themeRepository).findById(any(Long.class));
-        verify(themeRepository).existsByName(any(String.class));
-        verify(themeRepository).save(any(Theme.class));
-        assertThat(result).isNotNull();
-        assertThat(result).isEqualTo(themeToUpdate);
-    }
-
-    @Test
-    @DisplayName("Mettre à jour un thème sans changer le nom")
-    public void update_existingThemeWithSameName() {
-        // Given
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.of(theme));
-        when(themeRepository.save(any(Theme.class)))
-                .thenReturn(theme);
-
-        // When
-        Theme result = themeService.update(theme.getId(), theme);
-
-        // Then
-        verify(themeRepository).findById(any(Long.class));
-        verify(themeRepository, never()).existsByName(any(String.class));
-        verify(themeRepository).save(any(Theme.class));
-        assertThat(result).isNotNull();
-        assertThat(result).isEqualTo(theme);
     }
 
 
-    @Test
-    @DisplayName("Mettre à jour un thème avec ID discordant")
-    public void update_mismatchedId() {
-        // Given
-        Theme themeToUpdate = new Theme();
-        themeToUpdate.setId(2);
+    @Nested
+    @DisplayName("Tests pour create")
+    class CreateTests {
 
-        // When & Then
-        assertThrows(MismatchedIdException.class, () -> themeService.update(theme.getId(), themeToUpdate));
+        @Test
+        @DisplayName("Créer un nouveau thème")
+        public void create_newTheme() {
+            // Given
+            when(themeRepository.existsByName(any(String.class)))
+                    .thenReturn(false);
+            when(themeRepository.save(any(Theme.class)))
+                    .thenReturn(theme);
+
+            // When
+            Theme result = themeService.create(theme);
+
+            // Then
+            verify(themeRepository).existsByName(any(String.class));
+            verify(themeRepository).save(any(Theme.class));
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(theme);
+        }
+
+        @Test
+        @DisplayName("Créer un thème déjà existant")
+        public void create_existingTheme() {
+            // Given
+            when(themeRepository.existsByName(any(String.class)))
+                    .thenReturn(true);
+
+            // When & Then
+            assertThrows(AlreadyExistException.class, () -> themeService.create(theme));
+
+            verify(themeRepository).existsByName(any(String.class));
+        }
+
     }
 
-    @Test
-    @DisplayName("Mettre à jour un thème inexistant")
-    public void update_nonExistingTheme() {
-        // Given
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.empty());
 
-        // When & Then
-        assertThrows(NotFoundException.class, () -> themeService.update(theme.getId(), theme));
+    @Nested
+    @DisplayName("Tests pour update")
+    class UpdateTests {
 
-        verify(themeRepository).findById(any(Long.class));
+        @Test
+        @DisplayName("Mettre à jour un thème existant")
+        public void update_existingTheme() {
+
+            Theme themeToUpdate = createUpdatedTheme();
+
+            // Given
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.of(theme));
+            when(themeRepository.existsByName(any(String.class)))
+                    .thenReturn(false);
+            when(themeRepository.save(any(Theme.class)))
+                    .thenReturn(themeToUpdate);
+
+            // When
+            Theme result = themeService.update(theme.getId(), themeToUpdate);
+
+            // Then
+            verify(themeRepository).findById(any(Long.class));
+            verify(themeRepository).existsByName(any(String.class));
+            verify(themeRepository).save(any(Theme.class));
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(themeToUpdate);
+        }
+
+        @Test
+        @DisplayName("Mettre à jour un thème sans changer le nom")
+        public void update_existingThemeWithSameName() {
+            // Given
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.of(theme));
+            when(themeRepository.save(any(Theme.class)))
+                    .thenReturn(theme);
+
+            // When
+            Theme result = themeService.update(theme.getId(), theme);
+
+            // Then
+            verify(themeRepository).findById(any(Long.class));
+            verify(themeRepository, never()).existsByName(any(String.class));
+            verify(themeRepository).save(any(Theme.class));
+            assertThat(result).isNotNull();
+            assertThat(result).isEqualTo(theme);
+        }
+
+
+        @Test
+        @DisplayName("Mettre à jour un thème avec ID discordant")
+        public void update_mismatchedId() {
+            // Given
+            Theme themeToUpdate = new Theme();
+            themeToUpdate.setId(2);
+
+            // When & Then
+            assertThrows(MismatchedIdException.class, () -> themeService.update(theme.getId(), themeToUpdate));
+        }
+
+        @Test
+        @DisplayName("Mettre à jour un thème inexistant")
+        public void update_nonExistingTheme() {
+            // Given
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.empty());
+
+            // When & Then
+            assertThrows(NotFoundException.class, () -> themeService.update(theme.getId(), theme));
+
+            verify(themeRepository).findById(any(Long.class));
+        }
+
+        @Test
+        @DisplayName("Mettre à jour un thème avec un nom déjà existant")
+        public void update_existingThemeWithExistingName() {
+            // Given
+            Theme themeToUpdate = createUpdatedTheme();
+
+            when(themeRepository.findById(any(Long.class)))
+                    .thenReturn(Optional.of(theme));
+            when(themeRepository.existsByName(any(String.class)))
+                    .thenReturn(true);
+
+            // When & Then
+            assertThrows(AlreadyExistException.class, () -> themeService.update(theme.getId(), themeToUpdate));
+
+            verify(themeRepository).findById(any(Long.class));
+            verify(themeRepository).existsByName(any(String.class));
+        }
+
     }
 
-    @Test
-    @DisplayName("Mettre à jour un thème avec un nom déjà existant")
-    public void update_existingThemeWithExistingName() {
-        // Given
-        Theme themeToUpdate = createUpdatedTheme();
 
-        when(themeRepository.findById(any(Long.class)))
-                .thenReturn(Optional.of(theme));
-        when(themeRepository.existsByName(any(String.class)))
-                .thenReturn(true);
+    @Nested
+    @DisplayName("Tests pour delete")
+    class DeleteTests {
 
-        // When & Then
-        assertThrows(AlreadyExistException.class, () -> themeService.update(theme.getId(), themeToUpdate));
+        @Test
+        @DisplayName("Supprimer un thème existant")
+        public void delete_existingTheme() {
+            // Given
+            when(themeRepository.existsById(any(Long.class)))
+                    .thenReturn(true);
 
-        verify(themeRepository).findById(any(Long.class));
-        verify(themeRepository).existsByName(any(String.class));
-    }
+            // When
+            themeService.delete(theme.getId());
 
-    @Test
-    @DisplayName("Supprimer un thème existant")
-    public void delete_existingTheme() {
-        // Given
-        when(themeRepository.existsById(any(Long.class)))
-                .thenReturn(true);
+            // Then
+            verify(themeRepository).existsById(any(Long.class));
+            verify(themeRepository).deleteById(any(Long.class));
+        }
 
-        // When
-        themeService.delete(theme.getId());
+        @Test
+        @DisplayName("Supprimer un thème inexistant")
+        public void delete_nonExistingTheme() {
+            // Given
+            when(themeRepository.existsById(any(Long.class)))
+                    .thenReturn(false);
 
-        // Then
-        verify(themeRepository).existsById(any(Long.class));
-        verify(themeRepository).deleteById(any(Long.class));
-    }
+            // When & Then
+            assertThrows(NotFoundException.class, () -> themeService.delete(theme.getId()));
 
-    @Test
-    @DisplayName("Supprimer un thème inexistant")
-    public void delete_nonExistingTheme() {
-        // Given
-        when(themeRepository.existsById(any(Long.class)))
-                .thenReturn(false);
+            verify(themeRepository).existsById(any(Long.class));
+        }
 
-        // When & Then
-        assertThrows(NotFoundException.class, () -> themeService.delete(theme.getId()));
-
-        verify(themeRepository).existsById(any(Long.class));
     }
 
 }
