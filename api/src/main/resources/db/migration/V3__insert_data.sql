a85bb8dabe6a8c076ccef682185098126a4dd7bc
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-17 14:50:28 +0100

    Réorganisation de la migration flyway pour tenir compte de l'environnement des tests

--- api/src/main/resources/db/migration/V3__insert_data.sql
+++
@@ -1,8 +0,0 @@
-
-
--- Insertion de données dans la table "users"
-INSERT INTO users (username, email, password)
-VALUES
-    ('alice', 'alice@example.com', 'password123'),
-    ('bob', 'bob@example.com', 'password456');
-
---
+++ api/src/test/resources/db/migrations/test/V3__insert_data.sql
@@ -0,0 +1,17 @@
+
+
+-- Insertion de données dans la table "users"
+INSERT INTO users (username, email, password)
+VALUES
+    ('alice', 'alice@example.com', 'password123'),
+    ('bob', 'bob@example.com', 'password456');
+
+
+INSERT INTO themes (name)
+VALUES
+    ('Art'),
+    ('Droit & Politique'),
+    ('Histoire & Géographie'),
+    ('Littérature & Langue française'),
+    ('Sciences humaines'),
+    ('Sciences & Techniques')
