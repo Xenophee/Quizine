bb636827deec1c3a6913f3c6bb48cdc82c53e466
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-19 10:32:50 +0100

    Ajout de la dépendance mapstruct

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+    implementation("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 
