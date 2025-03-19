c2858921f79c7d04c079d19666be9eb1ec4133fe
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-19 19:08:20 +0100

    Mise à jour des exceptions avec un message par défaut

--- api/src/main/java/com/dassonville/api/exception/AlreadyExistException.java
+++ api/src/main/java/com/dassonville/api/exception/AlreadyExistException.java
@@ -1,7 +1,15 @@
 package com.dassonville.api.exception;
 
 public class AlreadyExistException extends RuntimeException {
+
+    // Constructeur avec message personnalisé
     public AlreadyExistException(String message) {
         super(message);
     }
+
+    // Constructeur avec message par défaut
+    public AlreadyExistException() {
+        super("L'élément existe déjà !");
+    }
 }
+
--- api/src/main/java/com/dassonville/api/exception/NotFoundException.java
+++ api/src/main/java/com/dassonville/api/exception/NotFoundException.java
@@ -1,7 +1,14 @@
 package com.dassonville.api.exception;
 
 public class NotFoundException extends RuntimeException {
+
+    // Constructeur avec message personnalisé
     public NotFoundException(String message) {
         super(message);
+    }
+
+    // Constructeur avec message par défaut
+    public NotFoundException() {
+        super("L'élément n'existe pas !");
     }
 }
