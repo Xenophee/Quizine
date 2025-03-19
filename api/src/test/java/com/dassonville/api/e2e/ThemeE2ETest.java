1e517ed950fb09ec78946b70d1fab4c92e82d826
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-03-19 19:14:32 +0100

    Mise en place de tests e2e

---
+++ api/src/test/java/com/dassonville/api/e2e/ThemeE2ETest.java
@@ -0,0 +1,109 @@
+package com.dassonville.api.e2e;
+
+import com.dassonville.api.dto.ThemeDTO;
+import com.dassonville.api.dto.ThemeUpsertDTO;
+import com.dassonville.api.model.Theme;
+import com.dassonville.api.repository.ThemeRepository;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ThemeE2ETest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ThemeRepository themeRepository; // Pour vérifier la BDD après insertion
+
+    @BeforeEach
+    public void clearDatabase(@Autowired Flyway flyway) {
+        flyway.clean();
+        flyway.migrate();
+    }
+
+    @Test
+    @DisplayName("Créer et récupérer le thème avec le header Location")
+    public void shouldCreateAndRetrieveTheme() {
+        // Given
+        ThemeUpsertDTO themeToCreate = new ThemeUpsertDTO("Cinéma", "Pour les cinéphiles");
+        String url = "http://localhost:" + port + "/api/themes";
+
+        // When - Envoi de la requête HTTP pour créer le thème
+        HttpEntity<ThemeUpsertDTO> request = new HttpEntity<>(themeToCreate);
+        ResponseEntity<ThemeDTO> response = restTemplate.exchange(url, HttpMethod.POST, request, ThemeDTO.class);
+
+        // Then - Vérifie la réponse HTTP
+        URI location = response.getHeaders().getLocation();
+        assertThat(location).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody().name()).isEqualTo(themeToCreate.name());
+
+        // Vérifie que le thème est bien stocké en base
+        boolean themeIsExists = themeRepository.existsByName(themeToCreate.name());
+        assertThat(themeIsExists).isEqualTo(true);
+
+        // Vérifie qu'on peut récupérer le thème avec un GET
+        ResponseEntity<ThemeDTO> getResponse = restTemplate.getForEntity(location, ThemeDTO.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(getResponse.getBody().name()).isEqualTo(themeToCreate.name());
+    }
+
+    @Test
+    @DisplayName("Mettre à jour un thème")
+    public void shouldUpdateTheme() {
+        // Given
+        long id = 1;
+        ThemeUpsertDTO themeToUpdate = new ThemeUpsertDTO("Cinéma", "Pour les cinéphiles");
+        String url = "http://localhost:" + port + "/api/themes/" + id;
+
+        // When - Envoi de la requête HTTP pour mettre à jour le thème
+        HttpEntity<ThemeUpsertDTO> requestUpdate = new HttpEntity<>(themeToUpdate);
+        ResponseEntity<ThemeDTO> responseUpdate = restTemplate.exchange(url, HttpMethod.PUT, requestUpdate, ThemeDTO.class);
+
+        // Then - Vérifie la réponse HTTP
+        assertThat(responseUpdate.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseUpdate.getBody().name()).isEqualTo(themeToUpdate.name());
+
+        // Vérifie que le thème est bien mis à jour en base
+        Optional<Theme> theme = themeRepository.findById(id);
+        assertThat(theme.get().getName()).isEqualTo(themeToUpdate.name());
+    }
+
+    @Test
+    @DisplayName("Mettre à jour un thème inexistant")
+    public void shouldFailToUpdateNonExistingTheme() {
+        // Given
+        long id = 9;
+        ThemeUpsertDTO themeToUpdate = new ThemeUpsertDTO("Cinéma", "Pour les cinéphiles");
+        String url = "http://localhost:" + port + "/api/themes/" + id;
+
+        // When - Envoi de la requête HTTP pour mettre à jour le thème
+        HttpEntity<ThemeUpsertDTO> requestUpdate = new HttpEntity<>(themeToUpdate);
+        ResponseEntity<Error> responseUpdate = restTemplate.exchange(url, HttpMethod.PUT, requestUpdate, Error.class);
+
+        // Then - Vérifie la réponse HTTP
+        assertThat(responseUpdate.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(responseUpdate.getBody().getMessage()).isNotEmpty();
+    }
+}
+
+
