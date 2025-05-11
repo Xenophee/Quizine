595b1fd48464eea955bb75a1878e821a10130584
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-05-11 21:18:27 +0200

    Mise à jour et enrichissement de méthodes

--- api/src/main/java/com/dassonville/api/repository/QuizRepository.java
+++ api/src/main/java/com/dassonville/api/repository/QuizRepository.java
@@ -1,16 +1,39 @@
 package com.dassonville.api.repository;
 
 import com.dassonville.api.model.Quiz;
+import com.dassonville.api.projection.PublicQuizProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
-    boolean existsByTitle(String title);
+    /*String FIND_ALL_ACTIVE_PUBLIC_QUIZZES = """
+    SELECT quiz.id, quiz.title, quiz.isVipOnly, quiz.createdAt,
+           category.id, category.name,
+           theme.id,
+           COUNT(question.id)
+    FROM Quiz quiz
+    JOIN quiz.category category
+    JOIN quiz.theme theme
+    LEFT JOIN quiz.questions question
+    WHERE quiz.disabledAt IS NULL
+    GROUP BY quiz.id, quiz.title, quiz.isVipOnly, quiz.createdAt,
+             category.id, category.name,
+             theme.id
+    """;*/
 
-    Optional<Quiz> findByTitle(String title);
+
+    boolean existsByTitleIgnoreCase(String title);
+
+    boolean existsByTitleIgnoreCaseAndIdNot(String title, long id);
+
+    /*@Query(FIND_ALL_ACTIVE_PUBLIC_QUIZZES)
+    List<PublicQuizProjection> findByDisabledAtIsNull();*/
+
+    List<Quiz> findAllByDisabledAtIsNotNull();
 }
