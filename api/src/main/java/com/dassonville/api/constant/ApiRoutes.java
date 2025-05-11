6df0b327e50ce61768f69bbc85b7d962900745f2
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-05-11 20:32:30 +0200

    Mise à jour des routes

--- api/src/main/java/com/dassonville/api/constant/ApiRoutes.java
+++ api/src/main/java/com/dassonville/api/constant/ApiRoutes.java
@@ -5,6 +5,7 @@ public class ApiRoutes {
     public static final String API = "/api";
     public static final String API_ADMIN = API + "/admin";
     public static final String ID = "/{id}";
+    public static final String VISIBILITY = ID + "/visibility";
 
     private ApiRoutes() {}
 
@@ -14,6 +15,8 @@ public class ApiRoutes {
         public static final String BY_ID = BASE + ID;
         public static final String ADMIN_THEMES = API_ADMIN + STRING;
         public static final String ADMIN_BY_ID = ADMIN_THEMES + ID;
+
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_THEMES + VISIBILITY;
 
         private Themes() {}
     }
@@ -25,6 +28,8 @@ public class ApiRoutes {
         public static final String ADMIN_CATEGORIES = API_ADMIN + STRING;
         public static final String ADMIN_BY_ID = ADMIN_CATEGORIES + ID;
 
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_CATEGORIES + VISIBILITY;
+
         private Categories() {}
     }
 
@@ -35,7 +40,46 @@ public class ApiRoutes {
         public static final String ADMIN_DIFFICULTY_LEVELS = API_ADMIN + STRING;
         public static final String ADMIN_BY_ID = ADMIN_DIFFICULTY_LEVELS + ID;
 
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_DIFFICULTY_LEVELS + VISIBILITY;
+
         private DifficultyLevels() {}
     }
 
+    public static class Quizzes {
+        public static final String STRING = "/quizzes";
+        public static final String BASE = API + STRING;
+        public static final String BY_ID = BASE + ID;
+        public static final String ADMIN_QUIZZES = API_ADMIN + STRING;
+        public static final String ADMIN_BY_ID = ADMIN_QUIZZES + ID;
+
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_QUIZZES + VISIBILITY;
+
+        private Quizzes() {}
+    }
+
+    public static class Questions {
+        public static final String STRING = "/questions";
+        public static final String BASE = API + STRING;
+        public static final String BY_ID = BASE + ID;
+        public static final String ADMIN_QUESTIONS = API_ADMIN + STRING;
+        public static final String ADMIN_BY_ID = ADMIN_QUESTIONS + ID;
+
+        public static final String ADMIN_QUESTIONS_POST = Quizzes.ADMIN_BY_ID + STRING;
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_QUESTIONS + VISIBILITY;
+
+        private Questions() {}
+    }
+
+    public static class Answers {
+        public static final String STRING = "/answers";
+        public static final String BASE = API + STRING;
+        public static final String BY_ID = BASE + ID;
+        public static final String ADMIN_ANSWERS = API_ADMIN + STRING;
+        public static final String ADMIN_BY_ID = ADMIN_ANSWERS + ID;
+
+        public static final String ADMIN_ANSWERS_POST = Questions.ADMIN_BY_ID + STRING;
+        public static final String ADMIN_VISIBILITY_PATCH = ADMIN_ANSWERS + VISIBILITY;
+
+        private Answers() {}
+    }
 }
