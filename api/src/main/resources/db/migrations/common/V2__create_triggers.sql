e178914434f1addfee9fc8c9d0b302360d6cc170
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-05-13 14:44:53 +0200

    Mise à jour de la base de données

--- api/src/main/resources/db/migrations/common/V2__create_triggers.sql
+++ api/src/main/resources/db/migrations/common/V2__create_triggers.sql
@@ -22,3 +22,30 @@ CREATE TRIGGER trg_single_reference_level
     BEFORE INSERT OR UPDATE ON difficulty_levels
     FOR EACH ROW
 EXECUTE FUNCTION ensure_single_reference_level();
+
+
+---------------------------------------------------
+-- Création du trigger pour s'assurer que la catégorie correspond au thème lors de l'insertion ou de la mise à jour d'un quiz
+---------------------------------------------------
+CREATE OR REPLACE FUNCTION validate_category_theme()
+    RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.id_category IS NOT NULL THEN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM categories
+            WHERE id = NEW.id_category AND id_theme = NEW.id_theme
+        ) THEN
+            RAISE EXCEPTION 'La catégorie (id_category) ne correspond pas au thème (id_theme)';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER check_category_theme
+    BEFORE INSERT OR UPDATE ON quizzes
+    FOR EACH ROW
+EXECUTE FUNCTION validate_category_theme();
