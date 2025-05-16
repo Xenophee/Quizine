f8e0bacf0a41437edb9a024b0bd544b71c8750a1
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-05-16 15:09:11 +0200

    Mise à jour de la valeur d'une constante

--- api/src/main/java/com/dassonville/api/constant/AppConstants.java
+++ api/src/main/java/com/dassonville/api/constant/AppConstants.java
@@ -5,5 +5,5 @@ package com.dassonville.api.constant;
 public class AppConstants {
 
     public static final int NEWNESS_THRESHOLD_DAYS = 30;
-    public static final int MINIMUM_QUIZ_QUESTIONS = 9;
+    public static final int MINIMUM_QUIZ_QUESTIONS = 10;
 }
