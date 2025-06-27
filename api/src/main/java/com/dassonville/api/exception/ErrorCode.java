efa50b58b4bc8f711625b77f5eb0b67b0950e63e
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-06-27 19:59:14 +0200

    Ajout de nouvelles erreurs possibles

--- api/src/main/java/com/dassonville/api/exception/ErrorCode.java
+++ api/src/main/java/com/dassonville/api/exception/ErrorCode.java
@@ -4,33 +4,38 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    THEME_NOT_FOUND("THEME_NOT_FOUND", "Le thème avec l'id %s est introuvable."),
-    THEME_ALREADY_EXISTS("THEME_ALREADY_EXISTS", "Un thème avec le nom « %s » existe déjà."),
-    THEME_CONTAINS_QUIZZES("THEME_CONTAINS_QUIZZES", "Le thème avec l'id %d contient des quiz et ne peut pas être supprimé."),
-    THEME_CONTAINS_NO_QUIZZES("THEME_CONTAINS_NO_QUIZZES", "Le thème avec l'id %d ne contient aucun quiz actif."),
+    THEME_NOT_FOUND("THEME_NOT_FOUND", "Le thème est introuvable."),
+    THEME_ALREADY_EXISTS("THEME_ALREADY_EXISTS", "Un thème avec le même nom existe déjà."),
+    THEME_CONTAINS_QUIZZES("THEME_CONTAINS_QUIZZES", "Le thème contient des quiz et ne peut pas être supprimé."),
+    THEME_CONTAINS_NO_QUIZZES("THEME_CONTAINS_NO_QUIZZES", "Le thème ne contient aucun quiz actif pour être activé."),
 
-    CATEGORY_NOT_FOUND("CATEGORY_NOT_FOUND", "La catégorie avec l'id %d est introuvable."),
-    CATEGORY_ALREADY_EXISTS("CATEGORY_ALREADY_EXISTS", "Une catégorie avec le nom %s existe déjà."),
+    CATEGORY_NOT_FOUND("CATEGORY_NOT_FOUND", "La catégorie est introuvable."),
+    CATEGORY_ALREADY_EXISTS("CATEGORY_ALREADY_EXISTS", "Une catégorie avec le même nom existe déjà."),
 
-    QUIZ_NOT_FOUND("QUIZ_NOT_FOUND", "Le quiz avec l'id %d est introuvable."),
-    QUIZ_ALREADY_EXISTS("QUIZ_ALREADY_EXISTS", "Un quiz avec le titre « %s » existe déjà."),
-    QUIZ_CONTAINS_NOT_ENOUGH_QUESTIONS("QUIZ_CONTAINS_NOT_ENOUGH_QUESTIONS", "Le quiz avec l'id %d doit contenir au moins %d questions pour être actif."),
+    QUIZ_NOT_FOUND("QUIZ_NOT_FOUND", "Le quiz est introuvable."),
+    QUIZ_ALREADY_EXISTS("QUIZ_ALREADY_EXISTS", "Un quiz avec le même titre existe déjà."),
+    QUIZ_CONTAINS_NOT_ENOUGH_QUESTIONS("QUIZ_CONTAINS_NOT_ENOUGH_QUESTIONS", "Le quiz doit contenir au moins %d questions pour être actif."),
 
-    QUESTION_NOT_FOUND("QUESTION_NOT_FOUND", "La question avec l'id %d est introuvable."),
-    QUESTION_ALREADY_EXISTS("QUESTION_ALREADY_EXISTS", "Une question avec le texte « %s » existe déjà."),
-    QUESTION_AND_QUIZ_MISMATCH("QUESTION_AND_QUIZ_MISMATCH", "La question avec l'id %d n'appartient pas au quiz avec l'id %d ou n'existe pas."),
+    QUESTION_NOT_FOUND("QUESTION_NOT_FOUND", "La question est introuvable."),
+    QUESTION_ALREADY_EXISTS("QUESTION_ALREADY_EXISTS", "Une question avec le même texte existe déjà."),
+    QUESTION_AND_QUIZ_MISMATCH("QUESTION_AND_QUIZ_MISMATCH", "La question n'appartient pas au quiz ou n'existe pas."),
+    QUESTION_AND_QUESTION_TYPE_MISMATCH("QUESTION_AND_QUESTION_TYPE_MISMATCH", "Les informations fournies ne correspondent pas au schéma attendu pour la question."),
+    QUESTION_TYPE_NOT_SUPPORTED("QUESTION_TYPE_NOT_SUPPORTED", "Le type du quiz n'autorise pas ce type de question."),
 
-    ANSWER_NOT_FOUND("ANSWER_NOT_FOUND", "La réponse avec l'id %d est introuvable."),
-    ANSWER_ALREADY_EXISTS("ANSWER_ALREADY_EXISTS", "Une réponse avec le texte « %s » existe déjà."),
-    ANSWERS_AND_QUESTION_MISMATCH("ANSWER_AND_QUESTION_MISMATCH", "Une ou plusieurs réponses avec les ids %d n'appartiennent pas à la question avec l'id %d ou n'existent pas."),
-    MINIMUM_ACTIVE_ANSWERS_NOT_REACHED("MINIMUM_ACTIVE_ANSWERS_NOT_REACHED", "La question avec l'id %d doit avoir au moins %d réponses actives."),
-    ATLEAST_ONE_CORRECT_ACTIVE_ANSWER_IS_MANDATORY("ATLEAST_ONE_CORRECT_ACTIVE_ANSWER_IS_MANDATORY", "La question avec l'id %d doit avoir au moins une réponse correcte et active."),
+    ANSWER_NOT_FOUND("ANSWER_NOT_FOUND", "La réponse est introuvable."),
+    ANSWER_ALREADY_EXISTS("ANSWER_ALREADY_EXISTS", "Une réponse avec le même texte existe déjà."),
+    ANSWERS_AND_QUESTION_MISMATCH("ANSWER_AND_QUESTION_MISMATCH", "Une ou plusieurs réponses n'appartiennent pas à la question ou n'existent pas."),
+    MINIMUM_ACTIVE_ANSWERS_NOT_REACHED("MINIMUM_ACTIVE_ANSWERS_NOT_REACHED", "Cette question doit avoir au moins %d réponses actives."),
+    AT_LEAST_ONE_CORRECT_ACTIVE_ANSWER_IS_MANDATORY("AT_LEAST_ONE_CORRECT_ACTIVE_ANSWER_IS_MANDATORY", "Une réponse correcte et active est requise pour cette question."),
+    ANSWER_TYPE_NOT_SUPPORTED("ANSWER_TYPE_NOT_SUPPORTED", "Ce type d'enregistrement n'est pas supporté pour cette question."),
 
-    DIFFICULTY_NOT_FOUND("DIFFICULTY_NOT_FOUND", "La difficulté avec l'id %d est introuvable."),
-    DIFFICULTIES_NOT_FOUND("DIFFICULTIES_NOT_FOUND", "Certains IDs fournis ne correspondent pas à des niveaux de difficulté existants : %s."),
-    DIFFICULTY_ALREADY_EXISTS("DIFFICULTY_ALREADY_EXISTS", "Une difficulté avec le nom « %s » existe déjà."),
-    DIFFICULTY_IS_REFERENCE("DIFFICULTY_IS_REFERENCE", "La difficulté avec l'id %d est une référence et ne peut pas être supprimée."),
+    DIFFICULTY_NOT_FOUND("DIFFICULTY_NOT_FOUND", "Le niveau de difficulté est introuvable."),
 
+    CHECK_ANSWER_TYPE_NOT_SUPPORTED("CHECK_ANSWER_NOT_SUPPORTED", "La vérification de la réponse ne prend pas en charge ce type de question."),
+
+    QUIZ_MISCONFIGURED("QUIZ_MISCONFIGURED", "Le quiz ne réunit pas les conditions nécessaires pour proposer des niveaux de difficulté."),
+
+    VALIDATION_ERROR("VALIDATION_ERROR", "Une ou plusieurs erreurs de validation ont été détectées."),
     INTERNAL_ERROR("INTERNAL_ERROR", "Une erreur interne empêche de traiter la requête."),
     UNEXPECTED_ERROR("UNEXPECTED_ERROR", "Une erreur inattendue s'est produite.");
 
