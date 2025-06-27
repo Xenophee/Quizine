2dccff0cefa68de9836039d6b9787d958ba2fe69
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-06-27 19:52:06 +0200

    Ajout d'une nouvelle exception spécifique

---
+++ api/src/main/java/com/dassonville/api/exception/MisconfiguredException.java
@@ -0,0 +1,17 @@
+package com.dassonville.api.exception;
+
+import lombok.Getter;
+
+@Getter
+public class MisconfiguredException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public MisconfiguredException(ErrorCode errorCode, Object... messageArgs) {
+        super(String.format(errorCode.getMessage(), messageArgs));
+        this.errorCode = errorCode;
+    }
+
+    public MisconfiguredException() {
+        this(ErrorCode.UNEXPECTED_ERROR);
+    }
+}
