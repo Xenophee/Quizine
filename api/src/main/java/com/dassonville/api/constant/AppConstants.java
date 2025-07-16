0484fb787a2442eb206cf5d35fcaf3797fceadd3
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-07-16 19:25:17 +0200

    Renommage et nettoyage

--- api/src/main/java/com/dassonville/api/constant/AppConstants.java
+++ api/src/main/java/com/dassonville/api/constant/AppConstants.java
@@ -5,7 +5,7 @@ package com.dassonville.api.constant;
 public class AppConstants {
 
     public static final int NEWNESS_THRESHOLD_DAYS = 30;
-    public static final int MINIMUM_QUIZ_QUESTIONS = 10;
+    public static final int MIN_ACTIVE_QUESTIONS_PER_QUIZ = 10;
 
     public static final String INSERT_SUFFIX = "_INSERT";
     public static final String UPDATE_SUFFIX = "_UPDATE";
@@ -13,9 +13,6 @@ public class AppConstants {
     public static final String CLASSIC_TYPE = "CLASSIC";
     public static final String TRUE_FALSE_TYPE = "TRUE_FALSE";
     public static final String MIXTE_TYPE = "MIXTE";
-
-    public static final String CLASSIC_QUESTION_TYPE = "CLASSIC";
-    public static final String TRUE_FALSE_QUESTION_TYPE = "TRUE_FALSE";
 
     public static final String CLASSIC_TEXT_ANSWER_TYPE = "CLASSIC_TEXT";
     public static final String CLASSIC_CHOICE_ANSWER_TYPE = "CLASSIC_CHOICE";
