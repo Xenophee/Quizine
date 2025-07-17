1dc66b851c2860fa0b10471cc0b5d30f9bb81bfd
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-07-17 22:37:19 +0200

    Utilitaire provisoire pour du jsonb

---
+++ api/src/main/java/com/dassonville/api/util/JsonbConverter.java
@@ -0,0 +1,39 @@
+package com.dassonville.api.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Converter
+public class JsonbConverter implements AttributeConverter<List<String>, String> {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        try {
+            return objectMapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Erreur de conversion en JSON", e);
+        }
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        try {
+            if (dbData == null) {
+                return new ArrayList<>();
+            }
+            return objectMapper.readValue(dbData, new TypeReference<List<String>>() {});
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Erreur de lecture du JSON", e);
+        }
+    }
+}
+
