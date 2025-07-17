52c17fc5d5ab5df8f193df48042a17702c4f6abf
Perrine Dassonville <perrine.dassonville@outlook.fr>
2025-07-17 21:55:31 +0200

    Retrait de l'exigence active

--- api/src/main/resources/db/migrations/common/trigger/V1005__check_game_rules_exists.sql
+++ api/src/main/resources/db/migrations/common/trigger/V1005__check_game_rules_exists.sql
@@ -9,14 +9,15 @@ BEGIN
     IF NOT EXISTS (SELECT 1
                    FROM game_rules
                    WHERE difficulty_level_id = NEW.difficulty_level_id
-                     AND question_type_code = NEW.question_type_code
-                     AND disabled_at IS NULL) THEN
-        RAISE EXCEPTION 'Impossible d''insérer la relation entre le type de question et niveau de difficulté : aucune règle de jeu active ne correspond à cette combinaison.';
+                     AND question_type_code = NEW.question_type_code) THEN
+        RAISE EXCEPTION 'Impossible d''insérer la relation entre le type de question et niveau de difficulté : aucune règle de jeu ne correspond à cette combinaison.';
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+
 
 
 CREATE TRIGGER trg_check_game_rule_exists
